2180e1611697198b5ebd604143c75294d4eb1edf
Bogdan Ungureanu <ubogdan@gmail.com>
2021-10-14 14:22:04 +0300

    http over sock5 proxy

---
+++ http-socks5/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+
+	"golang.org/x/net/proxy"
+)
+
+func main() {
+	myClient := http.Client{}
+
+	res, err := myClient.Get("https://ifconfig.co/ip")
+	if err != nil {
+		log.Fatalf("http.GET failed:%s", err)
+	}
+	defer res.Body.Close()
+
+	ip, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatalf("ioutil.Read failed %s", err)
+	}
+
+	log.Printf("IP %s", ip)
+
+	mainr()
+}
+
+func mainr() {
+	auth := proxy.Auth{
+		User:     os.Getenv("PROXY_USERNAME"),
+		Password: os.Getenv("PROXY_PASSWORD"),
+	}
+
+	dialer, err := proxy.SOCKS5("tcp", os.Getenv("PROXY_ADDRESS"), &auth, proxy.Direct)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "can't connect to the proxy:", err)
+	}
+
+	tr := http.Transport{Dial: dialer.Dial}
+	myClient := &http.Client{
+		Transport: &tr,
+	}
+
+	res, err := myClient.Get("https://ifconfig.co/ip")
+	if err != nil {
+		log.Fatalf("http.GET failed:%s", err)
+	}
+	defer res.Body.Close()
+
+	ip, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatalf("ioutil.Read failed %s", err)
+	}
+
+	log.Printf("IP %s", ip)
+}
